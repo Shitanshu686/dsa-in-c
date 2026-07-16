b557fe4c2bf56b03b393e9603b8745594867d08b
Shitanshu Jha <shitanshujha40@gmail.com>
2026-07-16 12:43:14 +0530

    Update stack.c

--- Stack/stack.c
+++ Stack/stack.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int stack[5]; // Stack ka size 5 hai
-int top = -1;  // Shuru me stack khaali hai
+int top = -1; // Shuru me stack khaali hai
 
 // Stack me value daalne ke liye
 void push(int value) {
@@ -33,17 +33,39 @@ void peek() {
     }
 }
 
+// Stack ki saari values dikhane ke liye
+void display() {
+    if (top == -1) {
+        printf("Stack Khaali hai!\n");
+    } else {
+        printf("Stack Elements: ");
+
+        for (int i = top; i >= 0; i--) {
+            printf("%d ", stack[i]);
+        }
+
+        printf("\n");
+    }
+}
+
 int main() {
     push(10);
     push(20);
     push(30);
-    
-    peek(); // 30 dikhayega
-    
-    pop();  // 30 nikal dega
-    pop();  // 20 nikal dega
-    
-    peek(); // Ab 10 dikhayega
-    
+
+    display();
+
+    peek();
+
+    pop();
+
+    display();
+
+    pop();
+
+    display();
+
+    peek();
+
     return 0;
 }
